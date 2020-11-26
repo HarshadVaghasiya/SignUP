57e7e9e127dd584fad1535cf1cefde57a12ad515
Harshad Vaghasiya <hmvaghasiya1215@gmail.com>
2020-11-26 12:42:17 +0530

    changed file

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post("/", function (req, res) {
 
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("server started listen at port 3000");
 });
 
